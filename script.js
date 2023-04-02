1856cd1882e1eebb980ffaf4792c030a2d2dd57b
tkellys <thamireskellysantos@gmail.com>
2023-04-02 18:02:40 -0300

    tarefaconcluida

--- script.js
+++ script.js
@@ -1,29 +1,50 @@
-function getCep(e){
-    let cep = e.value
-    var url = `https://viacep.com.br/ws/${cep}/json/`
-    console.log(url)
-    
-    fetch(url)
-    .then(response => response.jason()) {
-       return response.json();
-    })
-    .then(function(data){
-     return console.log(data);
-    })
+async function getCep() {
+
+    let cep = document.getElementById('inputCep');
+    try {
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep.value}/json/`);
+        var consultaCEPConvertida = await consultaCEP.json();
+        if (consultaCEPConvertida.erro) {
+            throw Error('CEP não existente!');
+          
+        }
+        var cidade = document.getElementById('inputLocalidade');
+        var logradouro = document.getElementById('inputLogradouro');
+        var bairro = document.getElementById('inputBairro');
+        var UF = document.getElementById('inputEstado');
+
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        bairro.value = consultaCEPConvertida.bairro;
+        UF.value = consultaCEPConvertida.uf;
+
+        console.log(consultaCEPConvertida);
+        return consultaCEPConvertida;
+
+    } catch (erro) {
+        
+    }
 }
-// getCep();
 
-
-let estado = document.getElementById("inputEstado").value;
-
-function getEstado(){
-    fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados/ce/distritos')
-    .then(function(response) {
-       return response.json();
-    })
-    .then(function(data){
-     return console.log(data);
-    })
+async function getEstado(){
+    var listaUF = await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados`);
+    var listaUFConvertida = await listaUF.json();
+    let estado = document.getElementById("inputEstado");
+    let siglas = "";
+    listaUFConvertida.forEach(municipio => {
+        siglas = siglas + `<option>${municipio.sigla}</option>`;        
+    });
+    estado.innerHTML = siglas;
 }
 
- getEstado();
+
+
+getEstado();// vou deixar pq a pagina ja inicia com a lista carregada.
+
+// getCep(); // nao precisa chamar pq vai ser chamado via html com o metodo onblur() quando eu inserir o cep e tirar o clique
+
+
+
+
+// //focusout - evento de clique fora do campo pelo javascript
+// cep.addEventListener('focusout', () => getCep(cep.value));
